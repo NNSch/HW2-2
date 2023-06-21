0ef10ba3f11dd27e44686e6883d49606173e1848
Natalia Shevchenko <ledidile@rambler.ru>
2023-06-21 14:03:24 +0300

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100; // баланс счёта до пополнения
-        int many = 1020; // сумма пополения баланса
+        int many = 100; // сумма пополения баланса
         int bonus = (many > 1000) ? (many / 100) : 0; // бонусные рубли
         int check = balance + many + bonus; // пополненный баланс счёта
         System.out.println("пополненный баланс счёта - " + check);
